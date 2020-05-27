7b17752e50ddb9f0193d1cc57ab148e4a748127e
fannyhasbi <fnyhsbi@gmail.com>
2020-05-27 22:30:34 +0700

    styling project page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>{{ $project->title }}</h1>
-  <p>{{ $project->description }}</p>
-  <a href="/projects">Go Back</a>
+  <header class="flex items-end mb-3">
+    <div class="flex justify-between items-end w-full">
+      <h2 class="text-grey text-sm font-normal">My Projects</h2>
+
+      <a href="/projects/create" class="button">New Project</a>
+    </div>
+  </header>
+  
+  <main>
+    <div class="lg:flex -mx-3">
+      <div class="lg:w-3/4 px-3 mb-6">
+        <div class="mb-8">
+          <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+          
+          <div class="card mb-3">Lorem ipsum.</div>
+          <div class="card mb-3">Lorem ipsum.</div>
+          <div class="card">Lorem ipsum.</div>
+        </div>
+
+        <div>
+          <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+          <textarea class="card w-full" style="min-height: 200px;">Lorem ipsum.</textarea>
+        </div>
+      </div>
+
+      <div class="lg:w-1/4 px-3">
+        @include('projects.card')
+      </div>
+    </div>
+  </main>
+
 @endsection
