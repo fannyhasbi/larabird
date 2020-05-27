15961553e2ccea7f32edb240a774d8e145d33a1e
fannyhasbi <fnyhsbi@gmail.com>
2020-05-27 22:01:50 +0700

    grid spacing and card tweaks

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="flex items-center mb-3">
-    <a href="/projects/create">New Project</a>
-  </div>
+  <header class="flex items-center mb-3">
+    <div class="flex justify-between items-center w-full">
+      <h2 class="text-grey text-sm font-normal">My Projects</h2>
 
-  <div class="flex">
+      <a href="/projects/create" class="button">New Project</a>
+    </div>
+  </header>
+
+  <div class="flex flex-wrap -mx-3">
     @forelse ($projects as $project)
-        <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
-          <h3 class="font-normal text-xl m-0 py-4">{{ $project->title }}</h3>
+      <div class="lg:w-1/3 px-3 pb-6">
+        <div class="bg-white mr-4 p-5 rounded-lg shadow" style="height: 200px">
+          <h3 class="font-normal text-xl m-0 py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+            <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+          </h3>
+
           <div class="text-grey">{{ Illuminate\Support\Str::limit($project->description, 100) }}</div>
         </div>
+      </div>
     @empty
         <div>No projects yet.</div>
     @endforelse
