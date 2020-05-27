d4efecbb062e1d5343f73b1065f164421e58427c
fannyhasbi <fnyhsbi@gmail.com>
2020-05-27 11:54:11 +0700

    testin Project model

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,10 +7,15 @@
   <title>Projects</title>
 </head>
 <body>
+  <h1>Birdboard</h1>
   <ul>
-    @foreach($projects as $project)
-      <li>{{ $project->title }}</li>
-    @endforeach
+    @forelse($projects as $project)
+      <li>
+        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+      </li>
+    @empty
+      <li>No projects yet.</li>
+    @endforelse
   </ul>
 </body>
 </html>
