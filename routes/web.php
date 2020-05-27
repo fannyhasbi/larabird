32dd3443bb119d80fc1f2ec859507bbf2c5f5d63
fannyhasbi <fnyhsbi@gmail.com>
2020-05-27 14:36:32 +0700

    Add laravel/ui for auth page

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,8 @@ Route::get('/', function () {
 
 Route::get('/projects', 'ProjectsController@index');
 Route::get('/projects/{project}', 'ProjectsController@show');
+Route::post('projects', 'ProjectsController@store')->middleware('auth');
 
-Route::post('projects', 'ProjectsController@store');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
