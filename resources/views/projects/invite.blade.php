d198b8bbb9d32131136e0e666bd5abb7f4df64da
fannyhasbi <fnyhsbi@gmail.com>
2020-06-01 14:58:41 +0700

    change blue-light to accent-light

--- resources/views/projects/invite.blade.php
+++ resources/views/projects/invite.blade.php
@@ -1,5 +1,5 @@
 <div class="card flex flex-col mt-3">
-  <h3 class="font-normal text-xl m-0 py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+  <h3 class="font-normal text-xl m-0 py-4 -ml-5 mb-3 border-l-4 border-accent-light pl-4">
     invite a user
   </h3>
 
