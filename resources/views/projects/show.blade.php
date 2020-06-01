3008f51d13082065cf824ac4d700d612b83cf5fa
fannyhasbi <fnyhsbi@gmail.com>
2020-06-01 13:30:59 +0700

    config tailwind theme

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
   <header class="flex items-end mb-3">
     <div class="flex justify-between items-end w-full">
-      <p class="text-grey text-sm font-normal">
-        <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+      <p class="text-default text-sm font-normal">
+        <a href="/projects" class="text-default text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
       </p>
 
       <div class="flex items-center">
@@ -23,7 +23,7 @@
     <div class="lg:flex -mx-3">
       <div class="lg:w-3/4 px-3 mb-6">
         <div class="mb-8">
-          <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+          <h2 class="text-lg text-default font-normal mb-3">Tasks</h2>
           
           @foreach ($project->tasks as $task)
             <div class="card mb-3">
@@ -32,7 +32,7 @@
                 @csrf
   
                 <div class="flex">
-                  <input type="text" name="body" value="{{ $task->body }}" class="w-full {{ $task->completed ? 'text-grey' : '' }}">
+                  <input type="text" name="body" value="{{ $task->body }}" class="bg-card text-default w-full {{ $task->completed ? 'text-default' : '' }}">
                   <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                 </div>
               </form>
@@ -43,13 +43,13 @@
             <form action="{{ $project->path() . '/tasks' }}" method="POST">
               @csrf
 
-              <input type="text" placeholder="Add a new task..." class="w-full" name="body">
+              <input type="text" placeholder="Add a new task..." class="bg-card text-default w-full" name="body">
             </form>
           </div>
         </div>
 
         <div>
-          <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+          <h2 class="text-lg text-default font-normal mb-3">General Notes</h2>
           <form action="{{ $project->path() }}" method="POST">
             @csrf
             @method('PATCH')
